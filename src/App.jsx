dfcdd0fffac7e6ddc4e32f60fccc6f67a9adb216
Wrickster <119149648+Wrickster@users.noreply.github.com.>
2025-01-11 19:19:03 -0500

    adding files

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./css/App.css";
+import Favorites from "./pages/Favorites";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import { MovieProvider } from "./contexts/MovieContext";
+import NavBar from "./components/NavBar";
+
+function App() {
+  const movieNumber = 1;
+  return (
+    <MovieProvider>
+      <NavBar></NavBar>
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/favorites" element={<Favorites></Favorites>}></Route>
+      </Routes>
+    </main>
+    </MovieProvider>
+  );
+}
+
+export default App;
